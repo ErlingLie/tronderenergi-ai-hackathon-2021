4e8352670bf0214f9cf120ca37270702a101d88c
ErlingLie <Erling.s.lie@gmail.com>
2021-03-14 10:37:42 +0100

    Evaluation script did not run correctly

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -12,7 +12,7 @@ from rye_flex_env.env import RyeFlexEnv
 from rye_flex_env.plotter import RyeFlexEnvEpisodePlotter
 from rye_flex_env.states import State, Action
 
-from nonLinearMpc import MPC_step
+from mpc import MPC_step
 from predictor import *
 
 import time
@@ -49,7 +49,7 @@ def main() -> None:
     env = RyeFlexEnv(data=data)
     plotter = RyeFlexEnvEpisodePlotter()
     data2 = pd.read_csv(join(root_dir, "data/train.csv"), index_col=0, parse_dates=True)
-    data3 = pd.concat([data2, data])
+    data = pd.concat([data2, data])
 
 
     # Reset episode to feb 2021, and get initial state
@@ -67,10 +67,12 @@ def main() -> None:
         #W_true = data.loc[env._time:env._time + N*env._time_resolution, "wind_production"]
         #C_true = data.loc[env._time:env._time + N*env._time_resolution, "consumption"]
         spot = data.loc[env._time:env._time + N*env._time_resolution, "spot_market_price"]
+        #print("State t: ", state[0] - state[1] - state[2] + action[0] + action[1])
 
         C = data.loc[env._time - 47*env._time_resolution:env._time, "consumption"]
         PV = data.loc[env._time - 47*env._time_resolution:env._time, "pv_production"]
         Wind = data.loc[env._time:env._time + N*env._time_resolution, "wind_speed_50m:ms"]
+        Wind_prod_last = data.loc[env._time, "wind_production"]
         C_estim = [np.array(C[-1])]
         PV_estim = [np.array(PV[-1])]
         for i in range(N):
@@ -80,10 +82,11 @@ def main() -> None:
             pv = get_predicted_solar_power(PV[-48:])
             PV_estim.append(pv)
             PV = np.concatenate([PV, pv])
-        W = []
-        for x in Wind:
-            W.append(get_predicted_wind_power(x))
-        W = np.array(W)
+        # W = []
+        # for x in Wind:
+        #     W.append(get_predicted_wind_power(x))
+        # W = np.array(W)
+        W = get_predicted_wind_power_stupid(Wind,Wind_prod_last,N)
         C = np.hstack(C_estim)
         #INSERT YOUR OWN ALGORITHM HERE
         action = MPC_step(N, state[3:6],PV[1:], W[1:], C[1:], spot[1:] )
