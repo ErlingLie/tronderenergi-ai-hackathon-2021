3f2190aa14f4887fa9d77274578eacf80d5c7ee3
ErlingLie <Erling.s.lie@gmail.com>
2021-03-14 10:28:19 +0100

    Eval script with estimated values

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -1,7 +1,7 @@
 """
 The test script that will be used for evaluation of the "agents" performance
 """
-from datetime import datetime
+from datetime import datetime, timedelta
 from os.path import abspath, dirname, join
 
 import gym
@@ -49,7 +49,9 @@ def main() -> None:
     env = RyeFlexEnv(data=data)
     plotter = RyeFlexEnvEpisodePlotter()
     data2 = pd.read_csv(join(root_dir, "data/train.csv"), index_col=0, parse_dates=True)
-    data = pd.concat([data2, data])
+    data3 = pd.concat([data2, data])
+
+
     # Reset episode to feb 2021, and get initial state
     state = env.reset(start_time=datetime(2021, 2, 1, 0, 0))
 
@@ -58,30 +60,33 @@ def main() -> None:
 
     info = {}
     done = False
-    N = 20
+    N = 30
     while not done:
         t0 = time.time()
-        PV = data.loc[env._time:env._time + N*env._time_resolution, "pv_production"]
-        W = data.loc[env._time:env._time + N*env._time_resolution, "wind_production"]
-        C = data.loc[env._time:env._time + N*env._time_resolution, "consumption"]
+        #PV_true = data.loc[env._time:env._time + N*env._time_resolution, "pv_production"]
+        #W_true = data.loc[env._time:env._time + N*env._time_resolution, "wind_production"]
+        #C_true = data.loc[env._time:env._time + N*env._time_resolution, "consumption"]
         spot = data.loc[env._time:env._time + N*env._time_resolution, "spot_market_price"]
-        #print("State t: ", state[0] - state[1] - state[2] + action[0] + action[1])
 
-        # C = data.loc[env._time - 47*env._time_resolution:env._time, "consumption"]
-        # Wind = data.loc[env._time:env._time + N*env._time_resolution, "wind_speed_50m:ms"]
-        # C_estim = [np.array(C[-1])]
-        # for i in range(N):
-        #     c = get_predicted_consumption(C[-48:])
-        #     C_estim.append(c)
-        #     C = np.concatenate([C, c])
-        # W = []
-        # for x in Wind:
-        #     W.append(get_predicted_wind_power(x))
-        # W = np.array(W)
-        # C = np.hstack(C_estim)
+        C = data.loc[env._time - 47*env._time_resolution:env._time, "consumption"]
+        PV = data.loc[env._time - 47*env._time_resolution:env._time, "pv_production"]
+        Wind = data.loc[env._time:env._time + N*env._time_resolution, "wind_speed_50m:ms"]
+        C_estim = [np.array(C[-1])]
+        PV_estim = [np.array(PV[-1])]
+        for i in range(N):
+            c = get_predicted_consumption(C[-48:])
+            C_estim.append(c)
+            C = np.concatenate([C, c])
+            pv = get_predicted_solar_power(PV[-48:])
+            PV_estim.append(pv)
+            PV = np.concatenate([PV, pv])
+        W = []
+        for x in Wind:
+            W.append(get_predicted_wind_power(x))
+        W = np.array(W)
+        C = np.hstack(C_estim)
         #INSERT YOUR OWN ALGORITHM HERE
         action = MPC_step(N, state[3:6],PV[1:], W[1:], C[1:], spot[1:] )
-        # action = np.array([0,0])
         print(env._time)
         print(time.time() - t0)
         state, reward, done, info = env.step(action)
