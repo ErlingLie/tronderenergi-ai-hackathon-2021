77d4469ee3c3ab4d7669e0d8781a32770a59cde6
ErlingLie <Erling.s.lie@gmail.com>
2022-03-17 18:38:06 +0100

    Change objective function to linprog
    Linprog better represents the actual costs.
    In addition, with linprog it is possible to represent the
    non-linear infinity norm by being creative with the inequality constraints.

--- scripts/mpc.py
+++ scripts/mpc.py
@@ -1,10 +1,12 @@
 import numpy as np
 from scipy import optimize
 from scipy import linalg
-from cvxopt import matrix, solvers
-
+from scipy import sparse
 def generateIQMatrics():
-    A = np.array([[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0]])
+    A = np.array([[1, 0, 0],
+                  [-1, 0, 0],
+                  [0, 1, 0],
+                  [0, -1, 0]])
     B = np.array([[0.85, 0, 0, 0], [ 0, 1, 0, 0], [0, 0, 0.325, 0], [0, 0, 0, 1]])
     C = np.zeros((4,7))
     C[:,3:] = -np.eye(4)
@@ -16,34 +18,42 @@ def generateIQMatrics():
     b = np.array([500, 0, 1670, 0, 0, 0, 0, 0, 400, 400, 55, 100])
     return A, b
 
-def genFullIQMatrix(N):
+def genFullIQMatrix(N, PV, W, C):
     A, b = generateIQMatrics()
     matrix = np.kron(np.eye(N), A)
     B = np.kron(np.ones(N), b)
+    Aiq2 = np.array([[0, 0, -1, 1, -1, 1, -1]])
+    A2 = np.kron(np.eye(N), Aiq2)
+    b2 = PV + W-C
+
+    matrix = np.vstack([matrix, A2])
+    B = np.hstack([B,b2])
+
     return matrix, B
 
 
 def generateEQInitalMatrix(x0):
-    A1 = np.array([[1, 0, 0, 0.85, -1, 0, 0], [0, 1, 0, 0, 0, 0.325, -1]])
-    A1[:, 3:] *= -1
+    A1 = np.array([[1, 0, 0, 0, 0, 0, 0],
+                   [0, 1, 0, 0, 0, 0, 0]])
     b0 = np.array([[1, 0, 0], [0,1,0]])@x0
     return A1, b0
-def generateEQMatrices(PV, W, C):
-    A1 = np.array([[1, 0, 0, 0.85, -1, 0, 0], [0, 1, 0, 0, 0, 0.325, -1]])
+def generateEQMatrices():
+    A1 = np.array([[1, 0, 0, 0.85, -1, 0, 0],
+                   [0, 1, 0, 0, 0, 0.325, -1]])
     B1 = np.zeros((2,7))
     B1[:2,:2] = -np.eye(2)
     Aeq1 = np.hstack([A1, B1])
     b1 = np.array([0,0])
 
-    Aeq2 = np.array([[0, 0, -1, 1, -1, 1, -1]])
-    b2 = np.array([PV + W - C])
+    # Aeq2 = np.array([[0, 0, -1, 1, -1, 1, -1]])
+    # b2 = np.array([PV + W - C])
 
-    Aeq2 = np.hstack([Aeq2, np.zeros((1,7))])
+    # Aeq2 = np.hstack([Aeq2, np.zeros((1,7))])
 
-    A = np.vstack([Aeq1, Aeq2])
-    b = np.hstack([b1, b2])
+    # A = np.vstack([Aeq1, Aeq2])
+    # b = np.hstack([b1, b2])
 
-    return A, b
+    return Aeq1, b1
 
 def genQ(N, spot):
     Q = []
@@ -53,8 +63,8 @@ def genQ(N, spot):
         Q.append(q)
     return linalg.block_diag(*Q)
 
-def genFullMatrix(N, x0, PV, W, C):
-    matrix = np.zeros(((N-1)*3+2, 7*N))
+def genFullMatrix(N, x0):
+    matrix = np.zeros(((N-1)*2+2, 7*N))
     A1, b0 = generateEQInitalMatrix(x0)
    # print(A.shape, b.shape, A1.shape, b0.shape)
     matrix[:2,:7] = A1
@@ -62,37 +72,43 @@ def genFullMatrix(N, x0, PV, W, C):
     r = 0
     B = b0
     for i in range(N-1):
-        pv = PV[i]
-        w = W[i]
-        cons = C[i]
-        A, b = generateEQMatrices(pv, w, cons)
-        matrix[c:c+3, r:r+14] = A
+        A, b = generateEQMatrices()
+        matrix[c:c+2, r:r+14] = A
         B = np.concatenate([B, b])
-        c += 3
+        c += 2
         r += 7
     return matrix, B
 
 
 
 
-def MPC_step(N, x0, PV, W, C, spot):
-    solvers.options['show_progress'] = False
+def MPC_step(N, x0, PV, W, C, spot, peak_power):
 
-    Aeq, Beq = genFullMatrix(N, x0, PV, W, C)
+    Aeq, Beq = genFullMatrix(N, x0,)
 
 
-    Aiq, Biq = genFullIQMatrix(N)
+    Aiq, Biq = genFullIQMatrix(N, PV, W, C)
 
     Q = genQ(N, spot)
-    P  = matrix(Q)
-    G = matrix(Aiq)
-    h = matrix(Biq)
-    A = matrix(Aeq)
-    b = matrix(Beq.reshape([-1]).astype(float))
-
-
-    x = solvers.qp(P, matrix(np.zeros(Q.shape[0]).reshape([-1,1])), G, h, A, b, kktsolver="chol")
+   
+    c = np.diag(Q)
+    c = np.concatenate([c,[49]])
+    At = np.kron(np.eye(N),np.array([[0,0,1,0,0,0,0]]))
+    At = np.hstack([At, -np.ones((N,1))])
+    At = np.vstack([At, np.zeros([1,At.shape[1]])])
+    At[-1,-1] = -1
+    Aiq = np.hstack([Aiq, np.zeros((Aiq.shape[0],1))])
+    Aeq = sparse.coo_matrix(np.hstack([Aeq, np.zeros((Aeq.shape[0],1))]))
+    Aiq = sparse.coo_matrix(np.vstack([Aiq,At]))
+    Biq = np.concatenate([Biq, np.zeros(N+1)])
+    Biq[-1] = -peak_power
+    x = optimize.linprog(c, Aiq, Biq, Aeq, Beq, method="highs-ipm")
+    # print(x["message"])
     val = x["x"][3:7]
+    # val = x["x"][:-1].reshape([-1,7])[:,3:7]
+    # val1 = x["x"][:-1].reshape([-1,7])
+    # print(x["fun"])
+    # return np.array([val[:,0]-val[:,1], val[:,2]-val[:,3]]), val1[:,0]
     return np.array((val[0]-val[1], val[2]-val[3]))
 
 
--- scripts/runMPC.py
+++ scripts/runMPC.py
@@ -21,8 +21,8 @@ def main() -> None:
     data = pd.read_csv(join(root_dir, "data/test.csv"), index_col=0, parse_dates=True)
     env = RyeFlexEnv(data=data)
     env.reset(start_time=datetime(2021, 2, 1, 0, 0))
-    data2 = pd.read_csv(join(root_dir, "data/train.csv"), index_col=0, parse_dates=True)
-    data = pd.concat([data2, data])
+    # data2 = pd.read_csv(join(root_dir, "data/train.csv"), index_col=0, parse_dates=True)
+    # data = pd.concat([data2, data])
     plotter = RyeFlexEnvEpisodePlotter()
 
     # INSERT YOUR OWN ALGORITHM HERE
@@ -33,38 +33,50 @@ def main() -> None:
     done = False
     # Initial state
     state = env._state.vector
-    N = 28
+    N = 12
+    print(env._time)
     while not done:
-        #PV = data.loc[env._time:env._time + N*env._time_resolution, "pv_production"]
-        #W = data.loc[env._time:env._time + N*env._time_resolution, "wind_production"]
-        #C = data.loc[env._time:env._time + N*env._time_resolution, "consumption"]
+        PV = data.loc[env._time:env._time + N*env._time_resolution, "pv_production"]
+        W = data.loc[env._time:env._time + N*env._time_resolution, "wind_production"]
+        C = data.loc[env._time:env._time + N*env._time_resolution, "consumption"]
         spot = data.loc[env._time:env._time + N*env._time_resolution, "spot_market_price"]
+        # PV = data.loc[env._time:env._episode_end_time, "pv_production"]
+        # W = data.loc[env._time:env._episode_end_time, "wind_production"]
+        # C = data.loc[env._time:env._episode_end_time, "consumption"]
+        # spot = data.loc[env._time:env._episode_end_time, "spot_market_price"]
         #print("State t: ", state[0] - state[1] - state[2] + action[0] + action[1])
 
-        C = data.loc[env._time - 47*env._time_resolution:env._time, "consumption"]
-        PV = data.loc[env._time - 47*env._time_resolution:env._time, "pv_production"]
-        Wind = data.loc[env._time:env._time + N*env._time_resolution, "wind_speed_50m:ms"]
-        Wind_prod_last = data.loc[env._time, "wind_production"]
-        C_estim = [np.array(C[-1])]
-        PV_estim = [np.array(PV[-1])]
-        for i in range(N):
-            c = get_predicted_consumption(C[-48:])
-            C_estim.append(c)
-            C = np.concatenate([C, c])
-            pv = get_predicted_solar_power(PV[-48:])
-            PV_estim.append(pv)
-            PV = np.concatenate([PV, pv])
-        # W = []
-        # for x in Wind:
-        #     W.append(get_predicted_wind_power(x))
-        # W = np.array(W)
-        W = get_predicted_wind_power_stupid(Wind,Wind_prod_last,N)
-        C = np.hstack(C_estim)
-        action = MPC_step(N, state[3:6],PV[1:], W[1:], C[1:], spot[1:] )
-        state, reward, done, info = env.step(action)
-        print(env._time)
-        plotter.update(info)
+        # C = data.loc[env._time - 47*env._time_resolution:env._time, "consumption"]
+        # PV = data.loc[env._time - 47*env._time_resolution:env._time, "pv_production"]
+        # Wind = data.loc[env._time:env._time + N*env._time_resolution, "wind_speed_50m:ms"]
+        # Wind_prod_last = data.loc[env._time, "wind_production"]
+        # C_estim = [np.array(C[-1])]
+        # PV_estim = [np.array(PV[-1])]
+        # for i in range(N):
+        #     c = get_predicted_consumption(C[-48:])
+        #     C_estim.append(c)
+        #     C = np.concatenate([C, c])
+        #     pv = get_predicted_solar_power(PV[-48:])
+        #     PV_estim.append(pv)
+        #     PV = np.concatenate([PV, pv])
+        # # W = []
+        # # for x in Wind:
+        # #     W.append(get_predicted_wind_power(x))
+        # # W = np.array(W)
+        # W = get_predicted_wind_power_stupid(Wind,Wind_prod_last,N)
+        # C = np.array(C_estim)
+        # PV = np.array(PV_estim)
 
+        # N = PV.shape[0]-1
+        # print(N)
+        action = MPC_step(N, state[3:6],PV[1:], W[1:], C[1:], spot[1:], state[-2] )
+        # break
+    # i = 0
+    # while not done:
+        state, reward, done, info = env.step(action)
+        # i += 1
+        # print(env._time)
+        plotter.update(info)
     print(f"Your test score is: {info['cumulative_reward']} NOK")
     plotter.plot_episode()
 
